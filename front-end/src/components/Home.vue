df30874853ec197a648c8636b60dda8fdc42c4f5
HWYD <1165203865@qq.com>
2021-11-29 19:35:25 +0800

    'feat:项目搭建：安装依赖和模块'

---
+++ front-end/src/components/Home.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+
+  <router-view></router-view>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
