1d5715ab09b74abe95d2904347c3cd58e3e8478d
HWYD <1165203865@qq.com>
2021-12-02 00:14:42 +0800

    'feat:封装axios'

---
+++ front-end/src/config/index.js
@@ -0,0 +1,24 @@
+/*
+环境配置
+*/
+const env = import.meta.env.MODE || 'prod'
+const EnvConfig = {
+  dev:{
+    baseApi:'/',
+    mockApi:'https://www.fastmock.site/mock/457de6ab62b48b914e5c767fcb117dbc/api'
+  },
+  test:{
+    baseApi:'/test.baseAPi',
+    mockApi:'https://www.fastmock.site/mock/457de6ab62b48b914e5c767fcb117dbc/api'
+  },
+  prod:{
+    baseApi:'/baseapi',
+    mockApi:'https://www.fastmock.site/mock/457de6ab62b48b914e5c767fcb117dbc/api'
+  }
+}
+
+export default {
+  env,
+  mock:true,
+  ...EnvConfig[env]
+}
--- front-end/src/main.js
+++ front-end/src/main.js
@@ -3,5 +3,8 @@ import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import request from './utils/request'
 
-createApp(App).use(router).use(ElementPlus).mount('#app')
+const app = createApp(App)
+app.config.globalProperties.$request =request
+app.use(router).use(ElementPlus).mount('#app')
