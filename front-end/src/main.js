2a48e6787a2feb83f4b042465eebed35cb983385
HWYD <1165203865@qq.com>
2021-12-14 00:03:47 +0800

    'feat:登录页面实现'

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -4,7 +4,9 @@ import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import request from './utils/request'
+import api from './api'
 
 const app = createApp(App)
 app.config.globalProperties.$request =request
+app.config.globalProperties.$api = api
 app.use(router).use(ElementPlus).mount('#app')
